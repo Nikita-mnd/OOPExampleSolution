a8cb11053594e398fa3dbbe015838cde5799037b
Nikita Nik <nikita.morozov.mnd@gmail.com>
2025-05-12 18:47:54 +0300

    ads

---
+++ ADSproject/Arraylist.cpp
@@ -0,0 +1,74 @@
+#include "Arraylist.h"
+
+ArrayList::ArrayList() {
+
+}
+ArrayList::~ArrayList() {
+	if (array != nullptr) {
+		delete[] array;
+	}
+}
+void ArrayList::add(int value) {
+
+	if (array != nullptr) {
+		int* newArray = new int[size+1];
+
+		for (int i = ; i < size; i++) {
+			newArray[i] = array[i]
+		}
+		delete[] array;
+		array = newArray;
+	}
+
+}
+void ArrayList::add(int index, int value) {
+
+
+
+}
+void ArrayList::addAll(int* values, int size) {
+
+
+
+}
+void ArrayList::addAll(int index, int* values, int size) {
+
+
+
+}
+
+void ArrayList::remove(int index) {
+
+
+
+}
+void ArrayList::remove() {
+
+
+
+}
+
+int ArrayList::size() {
+
+
+
+}
+int ArrayList::get(int index) {
+
+
+}
+void ArrayList::set(int index, int value) {
+
+
+
+}
+void ArrayList::clear() {
+
+
+}
+
+string ArrayList::toString() {
+
+
+
+}
---
+++ ADSproject/Arraylist.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+class ArrayList
+{
+private:
+
+public:
+	ArrayList();
+	~ArrayList();
+	void add(int value);
+	void add(int index, int value);
+	void addAll(int* values, int size);
+	void addAll(int index, int* values, int size);
+
+	void remove(int index);
+	void remove();
+
+	int size();
+	int get(int index);
+	void set(int index, int value);
+	void clear();
+	bool isEmpty() { return size() == 0; }
+
+	string toString();
+
+};
+
